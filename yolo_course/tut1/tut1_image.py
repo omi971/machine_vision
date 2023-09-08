90c3d8c92ef424543f8944ae189b087f8c439422
omi971 <nazmulhaqueomi971@gmail.com>
2023-09-09 03:53:05 +0600

    ok

--- yolo_course/tut1/tut1_image.py
+++ yolo_course/tut1/tut1_image.py
@@ -1,6 +1,12 @@
 import cv2
 from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
-resuts = model('image/1.png', show=True)
+
+# img = cv2.imread('image/2.png', 1)
+
+# cv2.imshow("Sample", img)
+# cv2.waitKey(5000)
+
+model = YOLO('../yolo_weights/yolov8n.pt')
+results = model('image/3.png', show=True)
 
 cv2.waitKey(0)

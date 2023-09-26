688abb82c4754d366b1e69b0e9962c07959cd3eb
omi971 <nazmulhaqueomi971@gmail.com>
2023-09-27 03:07:19 +0600

    yolo

--- yolo_course/tut2_(yolo_with_webcam)/tut2_(yolo_with_webcam).py
+++ yolo_course/tut2_(yolo_with_webcam)/tut2_(yolo_with_webcam).py
@@ -3,6 +3,38 @@ import cv2
 import cvzone
 import math
 
+
+def putTextRect(img, text, pos, scale=3, thickness=3, colorT=(255, 255, 255),
+                colorR=(255, 0, 255), font=cv2.FONT_HERSHEY_PLAIN,
+                offset=10, border=None, colorB=(0, 255, 0)):
+    """
+    Creates Text with Rectangle Background
+    :param img: Image to put text rect on
+    :param text: Text inside the rect
+    :param pos: Starting position of the rect x1,y1
+    :param scale: Scale of the text
+    :param thickness: Thickness of the text
+    :param colorT: Color of the Text
+    :param colorR: Color of the Rectangle
+    :param font: Font used. Must be cv2.FONT....
+    :param offset: Clearance around the text
+    :param border: Outline around the rect
+    :param colorB: Color of the outline
+    :return: image, rect (x1,y1,x2,y2)
+    """
+    ox, oy = pos
+    (w, h), _ = cv2.getTextSize(text, font, scale, thickness)
+
+    x1, y1, x2, y2 = ox - offset, oy + offset, ox + w + offset, oy - h - offset
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), colorR, cv2.FILLED)
+    if border is not None:
+        cv2.rectangle(img, (x1, y1), (x2, y2), colorB, border)
+    cv2.putText(img, text, (ox, oy), font, scale, colorT, thickness)
+
+    return img, [x1, y2, x2, y1]
+
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
@@ -47,7 +79,13 @@ while True:
             print('Confidence', conf)
 
             # Giving Error for the library-problem
-            cvzone.putTextRect(frame, f'{conf}', (max(0, x1), max(0, y1)))
+            # cv2.putText(frame, f'{conf}', x1, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255))
+            putTextRect(frame, f'{conf}', (max(0, x1), max(0, y1)))
+
+            # Class Name
+            cls = box.cls[0]
+            print(f'Class Name: {cls}')
+
     cv2.imshow('Webcam Stream', frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
